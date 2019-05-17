a057f17ac0299ac020d7ac847bbc8d6ab7d0b6d9
lixiaodonggg <997375155@qq.com>
2019-05-18 01:31:15 +0800

    ..

---
+++ src/main/java/thread/JoinThread.java
@@ -0,0 +1,40 @@
+package main.java.thread;
+
+public class JoinThread extends Thread{
+
+    /**
+     * main 0
+     * main 1
+     * 被join的线程 0
+     * 被join的线程 1
+     * 被join的线程 2
+     * 被join的线程 3
+     * 被join的线程 4
+     * main 2
+     * main 3
+     * main 4
+     * 执行完了被join的线程才开始执行主线程
+     * */
+
+
+    public JoinThread(String s) {
+        super(s);
+    }
+
+public static void main(String[] args) throws InterruptedException {
+    for (int i =0;i<5;i++){
+        if (i==2){
+            JoinThread l = new JoinThread("被join的线程");
+            l.start();
+            l.join();
+        }
+        System.out.println(Thread.currentThread().getName()+" "+i);
+    }
+}
+    public void run(){
+    for (int i = 0;i<5;i++){
+        System.out.println(getName()+" "+i);
+    }
+}
+
+}
---
+++ src/main/java/thread/YieldThread.java
@@ -0,0 +1,26 @@
+package main.java.thread;
+
+public class YieldThread extends Thread {
+    private boolean l;
+    public YieldThread(String name,boolean h){
+        super(name);
+        this.l = h;
+    }
+    public void run(){
+        for (int i = 0;i<100;i++){
+            System.out.println(getName()+" "+i);
+            if (i==2&&l){
+                yield();
+            }
+
+        }}
+
+    public static void main(String[] args) {
+        Thread h = new YieldThread("高",false);
+        h.setPriority(Thread.MAX_PRIORITY);
+        Thread l = new YieldThread("低",true);
+        l.setPriority(Thread.MIN_PRIORITY);
+        h.start();
+        l.start();
+    }
+}
